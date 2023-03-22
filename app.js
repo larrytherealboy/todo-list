31daeeb8d37190393620c1f60db47e9391bdbdb6
larrytherealboy <abclarryabclarry@gmail.com>
2023-03-22 16:05:39 +0800

    feat: add mongoosse

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
 // library espress, 建構應用程式伺服器
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose') // library mongoose
+
+// 僅在非正式環境時，使用 dotenv
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+// 設定連線到mongoose
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true})
+
+// 取得資料庫連線狀態
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 // 設定首頁路由
 app.get('/', (req, res) => {
