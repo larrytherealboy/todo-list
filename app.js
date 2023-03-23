a2f8b4fb70cbc1036f397e4986a464603d8ccc50
larrytherealboy <abclarryabclarry@gmail.com>
2023-03-23 14:43:02 +0800

    feat: modify index.hbs

--- app.js
+++ app.js
@@ -2,11 +2,10 @@
 const express = require('express')
 const app = express()
 const exhbs = require('express-handlebars')
-
 const mongoose = require('mongoose') // library mongoose
+const Todo = require('./models/todo') // 載入Todo model
 
-
-app.engine('hbs', exhbs({defaultLayout: 'main', extname: '.hbs'}))
+app.engine('hbs', exhbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 // 僅在非正式環境時，使用 dotenv
@@ -15,7 +14,7 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 // 設定連線到mongoose
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
 // 取得資料庫連線狀態
 const db = mongoose.connection
@@ -30,7 +29,10 @@ db.once('open', () => {
 
 // 設定首頁路由
 app.get('/', (req, res) => {
-  res.render('index')
+  Todo.find() // 取出 Todo model 裡的所有資料
+    .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+    .then(todos => res.render('index', { todos })) // 將資料傳給 index 樣板
+    .catch(error => console.error(error)) // 錯誤處理
 })
 
 // 設定 port 3000
