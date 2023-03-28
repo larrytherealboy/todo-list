67382bd01f8dec6c0cc5c8e039691d9181033aae
larrytherealboy <abclarryabclarry@gmail.com>
2023-03-28 22:08:11 +0800

    feat: model todo add isDone params

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use(bodyPaser.urlencoded({ extended: true }))
 // 設定路由
 app.get('/', (req, res) => {
   Todo.find() // 取出 Todo model 裡的所有資料
-    .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+    .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列（json 格式）
     .then(todos => res.render('index', { todos })) // 將資料傳給 index 樣板
     .catch(error => console.error(error)) // 錯誤處理
 })
@@ -69,11 +69,12 @@ app.get('/todos/:id/edit', (req, res) => {
 
 app.post('/todos/:id/edit', (req, res) => {
   const id = req.params.id
-  const name = req.body.name
+  const { name, isDone } = req.body
 
   return Todo.findById(id)
     .then(todo => {
       todo.name = name
+      todo.isDone = isDone === 'on'
       return todo.save()
     })
     .then(() => res.redirect(`/todos/${id}`))
