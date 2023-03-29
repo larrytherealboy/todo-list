55754c1289df6191681630df61dd0f695d33f749
larrytherealboy <abclarryabclarry@gmail.com>
2023-03-29 11:16:24 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,38 +1,19 @@
 // 載入 espress, 建構應用程式伺服器
 const express = require('express')
-const app = express()
 
 // 載入相關套件
 const exhbs = require('express-handlebars')
-const mongoose = require('mongoose') // library mongoose
 const bodyPaser = require('body-parser')
 const methodOverride = require('method-override')
 
 
 // 引用路由器
 const routes = require('./routes')
+// 引用mongoose連線設定
+require('./config/mongoose')
 
 
-
-// 僅在非正式環境時，使用 dotenv
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
-
-// 設定連線到mongoose
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-
-// 取得資料庫連線狀態
-const db = mongoose.connection
-// 連線異常
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-// 連線成功
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
-
+const app = express()
 
 // 設定樣版引擎
 app.engine('hbs', exhbs({ defaultLayout: 'main', extname: '.hbs' }))
