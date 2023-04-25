e955ea9c37698c192a615905c6b97f9b7665ef93
larrytherealboy <abclarryabclarry@gmail.com>
2023-04-25 15:26:53 +0800

    feat: add users modules

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -12,8 +12,8 @@ router.get('/new', (req, res) => {
 
 // 新增toods
 router.post('/', (req, res) => {
-  const name = req.body.name // 從 req.body 拿出表單裡的 name 資料
-  return Todo.create({ name }) // 存入資料庫
+  const todos = String(req.body.name).split(',').map(todo => ({ name: todo }))
+  return Todo.insertMany(todos) // 存入資料庫
     .then(() => res.redirect('/')) // 新增完成後導回首頁
     .catch(error => console.log(error))
 })
