a49777684841f1c05e66a4f15790136e18009a8b
larrytherealboy <abclarryabclarry@gmail.com>
2023-04-27 17:42:04 +0800

    feat: add logout feature

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -42,4 +42,9 @@ router.post('/register', (req, res) => {
     .catch(error => console.log(error))
 })
 
+router.get('/logout', (req, res) => {
+  req.logout()
+  res.redirect('/users/login')
+})
+
 module.exports = router
